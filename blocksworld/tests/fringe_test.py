61af5a3723387d5e6c3f218622c0208634cf415b
thanakorn <thanakorn.panyapiang@outlook.com>
2019-11-12 09:32:09 +0000

    Fix test

--- blocksworld/tests/fringe_test.py
+++ blocksworld/tests/fringe_test.py
@@ -25,7 +25,7 @@ class FringeTest(unittest.TestCase):
         f.add([node])
         front = f.remove_front()
         self.assertTrue(f.is_empty())
-        self.assertTrue(front.state.compare(board))
+        self.assertTrue(front.state == board)
 
     def test_fringe_order(self):
         f = Fringe()
@@ -35,8 +35,8 @@ class FringeTest(unittest.TestCase):
         board_2 = BoardState( (2,2), (1,1), (0,0), (2,1), (3,3))
         node_2 = Node(board_2, 0, 0, Action.Unknown)
         f.add([node_2])
-        self.assertTrue(f.remove_front().state.compare(board_1))
-        self.assertTrue(f.remove_front().state.compare(board_2))
+        self.assertTrue(f.remove_front().state == board_1)
+        self.assertTrue(f.remove_front().state == board_2)
 
     def test_reverse_fringe_order(self):
         f = ReverseFringe()
@@ -46,8 +46,8 @@ class FringeTest(unittest.TestCase):
         board_2 = BoardState( (2,2), (1,1), (0,0), (2,1), (3,3))
         node_2 = Node(board_2, 0, 0, Action.Unknown)
         f.add([node_2])
-        self.assertTrue(f.remove_front().state.compare(board_2))
-        self.assertTrue(f.remove_front().state.compare(board_1))
+        self.assertTrue(f.remove_front().state == board_2)
+        self.assertTrue(f.remove_front().state == board_1)
 
     def test_cost_ordered_fringe(self):
         f = CostOrderedFringe()
@@ -59,10 +59,10 @@ class FringeTest(unittest.TestCase):
         node_3 = Node(board, 15, 0, Action.Unknown)
         node_4 = Node(board, 0, 0, Action.Unknown)
         f.add([node_3, node_4])
-        self.assertTrue(f.remove_front().compare(node_4))
-        self.assertTrue(f.remove_front().compare(node_1))
-        self.assertTrue(f.remove_front().compare(node_3))
-        self.assertTrue(f.remove_front().compare(node_2))
+        self.assertTrue(f.remove_front() == node_4)
+        self.assertTrue(f.remove_front() == node_1)
+        self.assertTrue(f.remove_front() == node_3)
+        self.assertTrue(f.remove_front() == node_2)
 
 if __name__ == '__main__':
     unittest.main()
